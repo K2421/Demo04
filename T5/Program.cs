801eb67d464af102a249a10ddf3020e7cf5c2212
K2421 <viivi_2@hotmail.com>
2016-02-09 15:21:48 +0200

    Tehtävä 5

---
+++ T5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Radio radio = new Radio();           
+
+            radio.Switchon = true;           
+            radio.Volume = 5;
+            radio.Frequency = 21000.0;
+            Console.WriteLine("Radio is ON. Volume is {0} and frequancy is {1} ", radio.Volume, radio.Frequency);
+            Console.WriteLine();
+
+            radio.Switchon = true;          
+            radio.Volume = 10;
+            radio.Frequency = 27000.0;
+            Console.WriteLine("Radio is ON. Volume is {0} and frequancy is {1} ", radio.Volume, radio.Frequency);
+            Console.WriteLine();
+
+            radio.Switchon = true;
+            radio.Volume = 0;
+            radio.Frequency = 1000.0;
+            Console.WriteLine("Radio is ON. Volume is {0} and frequancy is {1} ", radio.Volume, radio.Frequency);
+            Console.WriteLine();
+            
+            radio.Switchon = false;          
+            radio.Volume = 7;
+            radio.Frequency = 2500.0;
+            Console.WriteLine("Radio is OFF, your options for volume and frequency aren't valid.");
+            Console.WriteLine();
+        }   
+    }
+}
---
+++ T5/radio.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T5
+{
+    class Radio
+    {
+        // field variables
+        private readonly double maxFrequency = 26000.0;
+        private readonly double minFrequency = 2000.0;
+        private readonly int maxVolume = 9;
+
+        private double frequency;
+        private int volume;
+        
+
+        //On/Off
+        public bool Switchon;
+
+        
+
+//Volume
+public int Volume
+        {
+            get
+            { return volume;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Volume is mute");
+                    volume = 0;
+                }
+                else if (value > maxVolume)
+                {
+                    Console.WriteLine("Volume is as high as possible!");
+                   volume = maxVolume;
+                }
+                else
+                {
+                    Console.WriteLine("You switched volume to " + value);
+                    
+                        volume = value;
+                }
+            }
+        }
+        public double Frequency
+        {
+            get
+            {
+                return frequency;
+            }
+            set
+            {
+                if (value < minFrequency)
+                {
+                    Console.WriteLine("There is no such a frequency - set it between 2000.0 - 26000.0");
+                    frequency = minFrequency;
+                }
+                else if (value > maxFrequency)
+                {
+                    Console.WriteLine("There is no such a frequency - set it between 2000.0 - 26000.0");
+                    frequency = maxFrequency;
+                }
+                else
+                {
+                    Console.WriteLine("The frequency is now " + value);
+                    frequency = value;
+                }
+            }
+        }
+
+
+    }
+
+    }
+
+
